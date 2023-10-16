354d35beb37c4c4a583c0a93ef8e0252a71ef79d
k0ChiyaSAnae <88915238+k0ChiyaSAnae@users.noreply.github.com>
2023-10-16 05:28:19 +0000

    update

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -5,14 +5,14 @@
 
 // I AM NOT DONE
 
-#[rustfmt::skip]
+//#[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
